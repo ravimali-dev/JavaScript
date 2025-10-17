cc062b451ee5817b8761486a5021414a7d366e70
Ravi Malli <ravimalio9887@gmail.com>
2025-10-17 14:14:20 +0530

    first commit

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
+
+console.log(((a, b) => {
+    return a + b;
+})(3, 5));
