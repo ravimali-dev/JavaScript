3373c50b7f1f1039ebdded6b48f2e3f0c584defa
Ravi Mali <ravimalakar091@gmail.com>
2026-05-03 15:55:57 +0530

    practice fetch

--- 07_Asyns/promise.js
+++ 07_Asyns/promise.js
@@ -41,23 +41,31 @@
 //     console.log(user.username);
 // })
 
-const promiseFour = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = false;
-        if (!error) {
-            resolve({username: "Ravi", password: "123"})
-        } else {
-            reject('ERROR: Something went wrong')
-        }
-    }, 1000)
-})
+// const promiseFour = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = false;
+//         if (!error) {
+//             resolve({username: "Ravi", password: "123"})
+//         } else {
+//             reject('ERROR: Something went wrong')
+//         }
+//     }, 1000)
+// })
 
- promiseFour
- .then((user) => {
-    console.log(user);
-    return user.password
-}).then((pass) => {
-    console.log(pass);
-}).catch(function(error){
-    // console.log(error);
-}).finally(() => console.log("The promise is either resolved or rejected"))
+//  promiseFour
+//  .then((user) => {
+//     console.log(user);
+//     return user.password
+// }).then((pass) => {
+//     console.log(pass);
+// }).catch(function(error){
+//     // console.log(error);
+// }).finally(() => console.log("The promise is either resolved or rejected"))
+
+async function myData()  {
+    let req = await fetch('https://api.github.com/users/hiteshchoudhary');
+    let res = await req.json();
+    console.log(res.name)
+}
+myData();
+
