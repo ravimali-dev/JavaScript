6d62346e52a91453eef0cb15733c1991223ac3be
Ravi Mali <ravimalakar091@gmail.com>
2026-05-06 00:15:42 +0530

    get_set in js

--- 08_oops_class/call.js
+++ 08_oops_class/call.js
@@ -1,13 +1,33 @@
-function SetUsername(username){
-    this.username = username
-}
+// function SetUsername(username){
+//     this.username = username
+// }
 
-function createUser(username, email, password){
-    SetUsername.call(this, username) // ✅ fix
+// function createUser(username, email, password){
+//     SetUsername.call(this, username) // ✅ fix
     
-    this.email = email
-    this.password = password
-}
+//     this.email = email
+//     this.password = password
+// }
 
-const chai = new createUser("chai", "chai@fb.com", "123")
-console.log(chai);
+// const chai = new createUser("chai", "chai@fb.com", "123")
+// console.log(chai);
+
+class React {
+        constructor(){
+            this.library = "React"
+            this.server = "https://localhost:300"
+
+            //requirement
+            document
+                .querySelector('button')
+                .addEventListener('click', this.handleClick.bind(this))
+
+        }
+        handleClick(){
+            console.log("button clicked");
+            console.log(this.server);
+        }
+    }
+
+    const app = new React();
+    // app.handleClick()
